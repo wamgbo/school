16b5f8c3b95c2fcf0e1e5f097a28945e1464fd0e
wang_bo <87056038+wamgbo@users.noreply.github.com>
2021-11-05 11:19:32 +0800

    Update 最近的閏年.cpp

--- code/最近的閏年.cpp
+++ code/最近的閏年.cpp
@@ -2,14 +2,21 @@
 using namespace std;
 
 main(){
-	int a;
+	int a,i;
 	cin>>a; 
-	if(a%4==0 and a%100!=0 or a%400==0){
-		cout<<"閏年";
-	}
-	else 
-	cout<<"平年";
 	
+	for(i=a+1;i<=a+100;i++){
+		if(i%4==0 and i%100!=0 or i%400==0){
+			cout<<i<<endl;
+			break;
+		}
+	}
+	for(i=a-1;i>=a-100;i--){
+		if(i%4==0 and i%100!=0 or i%400==0){
+			cout<<i<<endl;
+			break;
+		}
+	}
 	
 	
 	
