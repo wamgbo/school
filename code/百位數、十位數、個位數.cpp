d5bb33fe38eaea645ffb260aca4fd2f2b398e84d
wang_bo <87056038+wamgbo@users.noreply.github.com>
2021-11-09 21:27:35 +0800

    Update 百位數、十位數、個位數.cpp

--- code/百位數、十位數、個位數.cpp
+++ code/百位數、十位數、個位數.cpp
@@ -4,16 +4,25 @@ using namespace std;
 
 int main(int argc, char** argv) {
 	string a;
-	cin>>a;
-	if(a.size()>=2){
-	cout<<"百位數:"<<a[a.size()-3];
+	cin >> a;
+	if (a.size() > 2) {
+		cout << "百位數:" << a[a.size() - 3];
 	}
-	else{
-		cout<<"百位數:"<<a[a.size()-3];
+	else {
+		cout << "百位數:" << 0;
 	}
-	cout<<"十位數:"<<a[a.size()-2];
-	cout<<"個位數:"<<a[a.size()-1];
+	if (a.size() > 1) {
+		cout << "十位數:" << a[a.size() - 2];
+	}
+	else {
+		cout << "十位數:" << 0;
+	}
+	if (a.size() > 0) {
+		cout << "一位數:" << a[a.size() - 1];
+	}
+	else {
+		cout << "一位數:" << 0;
+	}
 	
 	return 0;
 }
-	
