cb6d0f5fd548d2ea77586c08b9943c67e91de08b
wang_bo <87056038+wamgbo@users.noreply.github.com>
2021-11-21 14:24:39 +0800

    可重複

--- code/判斷質數.cpp
+++ code/判斷質數.cpp
@@ -4,23 +4,25 @@ using namespace std;
 int main(int argc, char** argv) {
 	
 	int a,b=0,i;
-	cin>>a;
-	for(i=2;i<=a/2;i++){
-	if(a%i==0){
-		b++;
-		break;
-	} 
-	}
-	if(b==0){
-		cout<<"是質數";
-	}
-	else{
-		cout<<"不是質數";
-	}
+	while(cin>>a){
+		for(i=2;i<=a/2;i++){
+			if(a%i==0){
+				b++;
+				break;
+			} 
+		}
+			if(b==0){
+				cout<<"是質數"<<endl;
+			}
+			else{
+				cout<<"不是質數"<<endl;
+				}	
+		b=0;
+		}
+		
 
 
 
 
 	return 0;
 }
-
