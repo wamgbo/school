0463ee1cb7f264839ed73f387ce08b2ab4e64428
wang_bo <87056038+wamgbo@users.noreply.github.com>
2021-12-03 15:40:25 +0800

    Update 任意bit格雷碼轉換.cpp

--- code/任意bit格雷碼轉換.cpp
+++ code/任意bit格雷碼轉換.cpp
@@ -6,16 +6,14 @@ int main(int argc, char** argv) {
 	string a;
 	int b,c,i;
 	cin>>a;
-	cout<<a[0]; 
-	for(i=0;i<a.size()-1;i++){
-		if(a[i]!=a[i+1]){
+	cout<<a[0];
+	for(i=0; i<a.size()-1; i++) {
+		if(a[i]!=a[i+1]) {
 			cout<<1;
-		}	
-		else
-		cout<<0;
+		} else
+			cout<<0;
 	}
-	
-	
+
+
 	return 0;
 }
-
