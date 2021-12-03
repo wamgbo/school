17d2ba83513d21ad27be76b434ac687f387a14ca
wang_bo <87056038+wamgbo@users.noreply.github.com>
2021-12-03 15:40:07 +0800

    Update 判斷三角形.cpp

--- code/判斷三角形.cpp
+++ code/判斷三角形.cpp
@@ -2,15 +2,13 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-	
+
 	int a,b,c,i;
 	cin>>a>>b>>c;
-	if(a*a==c*c+b*b or b*b==c*c+a*a or c*c==a*a+b*b){
-		cout<<"啊可以"; 
-	}
-	else 
-	cout<<"啊不可以";
+	if(a*a==c*c+b*b or b*b==c*c+a*a or c*c==a*a+b*b) {
+		cout<<"啊可以";
+	} else
+		cout<<"啊不可以";
 
 	return 0;
 }
-
