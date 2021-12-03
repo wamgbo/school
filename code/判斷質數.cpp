13dc7d0415037370a3d69c6168494dd8092917c3
wang_bo <87056038+wamgbo@users.noreply.github.com>
2021-12-03 15:41:20 +0800

    Update 判斷質數.cpp

--- code/判斷質數.cpp
+++ code/判斷質數.cpp
@@ -2,24 +2,23 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-	
+
 	int a,b=0,i;
-	while(cin>>a){
-		for(i=2;i<=a/2;i++){
-			if(a%i==0){
+	while(cin>>a) {
+		for(i=2; i<=a/2; i++) {
+			if(a%i==0) {
 				b++;
 				break;
-			} 
-		}
-			if(b==0){
-				cout<<"是質數"<<endl;
 			}
-			else{
-				cout<<"不是質數"<<endl;
-				}	
-		b=0;
 		}
-		
+		if(b==0) {
+			cout<<"是質數"<<endl;
+		} else {
+			cout<<"不是質數"<<endl;
+		}
+		b=0;
+	}
+
 
 
 
