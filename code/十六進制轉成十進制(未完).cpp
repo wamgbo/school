abd25f3583cc6e05897be560311c39bf6ff340de
wang_bo <87056038+wamgbo@users.noreply.github.com>
2021-12-03 12:06:57 +0800

    Create 十六進制轉成十進制(未完).cpp

---
+++ code/十六進制轉成十進制(未完).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string.h>
+#include <math.h>
+using namespace std;
+int main(int argc, char** argv) {
+	int i,point;
+	float b,c;
+	string a;
+	cin>>a;
+	for(i=0;i<=a.size()-1;i++){
+		if(a[i]=="."){
+			point 
+		}
+	
+	
+	} 
+	
+	return 0;
+}
