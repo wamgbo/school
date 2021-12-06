25ef4753c930777f1b44d4ffdf1b0a8b31cdda5b
wang_bo <87056038+wamgbo@users.noreply.github.com>
2021-12-06 17:30:57 +0800

    Update 十六進制轉成十進制(未完).cpp

--- code/十六進制轉成十進制(未完).cpp
+++ code/十六進制轉成十進制(未完).cpp
@@ -25,18 +25,18 @@ int main(int argc, char** argv) {
 		}
 	}
 	for(i=0;i<point;i++){
-		if((int)a[i]>=65 and (int)a[i]<=90){
+		if((int)a[i]>='A' and (int)a[i]<='Z'){
 			c+=((int)a[i]-55)*pow(16,point-i-1); 
 		} 
-		if((int)a[i]>=48 and (int)a[i]<=57)
+		if((int)a[i]>='0' and (int)a[i]<='9')
 			c+=((int)a[i]-48)*pow(16,point-i-1);
 	}	
-		for(i=point+1;i>a.size();i++){
-			if((int)a[i]>=65 and (int)a[i]<=90){
-				c+=((int)a[i]-55)*pow(16,i-point); 
+		for(i=point+1;a.size()-1;i++){
+			if((int)a[i]>='A' and (int)a[i]<='Z'){
+				c+=((int)a[i]-55)*pow(16,point-i); 
 			} 
-			if((int)a[i]>=48 and (int)a[i]<=57)
-				c+=((int)a[i]-48)*pow(16,i-point);
+			if((int)a[i]>='0' and (int)a[i]<='9')
+				c+=((int)a[i]-48)*pow(16,point-i);
 		}	
 	cout<<c;
 	return 0;
