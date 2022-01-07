65f6ac2f994ec7e6b57ae8f2b5451543ea61d15a
wang_bo <87056038+wamgbo@users.noreply.github.com>
2022-01-07 10:41:59 +0800

    Update and rename 作業21 - 蝸牛爬樹.cpp to 蝸牛爬樹.cpp

---
+++ code/蝸牛爬樹.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+	int h,a,b,now_h=0,day=0;
+	cin>>h>>a>>b;
+	while(1){	
+		now_h+=a;
+		if(now_h<h){
+			now_h-=b;
+		}
+		day+=1; 
+		if(now_h>=h){
+			break;
+		}
+	}
+	cout<<day;
+}
