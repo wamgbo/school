4dab120611d4963624174697cf835f647f00e504
wang_bo <87056038+wamgbo@users.noreply.github.com>
2022-04-18 13:15:08 +0800

    Add files via upload

---
+++ web/webdec05/text.js
@@ -0,0 +1,17 @@
+function go(){
+    var a=1;    
+    alert(a);
+}
+function go1(){
+    var x = document.getElementById("in").value;
+    var y = document.getElementById("out1");
+    var z = document.getElementById("out2");
+    y.innerHTML = x + " 您好~";
+    z.textContent = "haaaaaaaaaaaaaaaaaaaaaaaaaaaaa!";
+}
+function trans(x) {
+    x.src="gif/dumbass.gif";
+}
+function normal(x) {
+    x.src="pic/dumbass.png";
+}
