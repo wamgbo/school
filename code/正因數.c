23c8cc801b0c293eaad120f768f7aeebaec84583
wang_bo <87056038+wamgbo@users.noreply.github.com>
2023-10-11 10:45:19 +0800

    Create 正因數.c

---
+++ code/正因數.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void factor(int n) {
+	for (int i = 1; i <= n; i++) {
+		if (n % i==0) {
+			printf("%d",i);
+			printf(" ");
+		}
+	}
+}
+int main() {
+	int n;
+	while (scanf("%d",&n)!=EOF)
+	{
+		printf("%d 的正因數為:", n);
+		factor(n);
+		printf("\n");
+		
+	}
+}
