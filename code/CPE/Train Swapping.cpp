8787fe2765bcd5c9c1d0529e5caaf8dbe8884c99
wang_bo <87056038+wamgbo@users.noreply.github.com>
2023-11-20 20:12:14 +0800

    cpe

---
+++ code/CPE/Train Swapping.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int bubbleSort(int array[], int n)
+{
+    bool swapped = true;
+    int j = 0;
+    int temp,count=0;
+
+    while (swapped)
+    {
+        swapped = false;
+        j++;
+        for (int i = 0; i < n - j; ++i)
+        {
+            if (array[i] > array[i + 1])
+            {
+                temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+                swapped = true;
+                count++;
+            }
+        }
+    }
+    return count;
+}
+int main() {
+    int time,num,array[100];
+    scanf("%d", &time);
+    for (int i = 0; i < time; i++)
+    {
+        scanf("%d", &num);
+        for (int j = 0; j < num; j++) {
+            scanf("%d", &array[j]);
+            
+        }
+        printf("Optimal train swapping takes %d swaps.\n", bubbleSort(array, num));
+
+    }
+}
