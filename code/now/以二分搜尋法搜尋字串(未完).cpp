3c65d3231a77ad0ee73a632764bd444152dcce10
wang_bo <87056038+wamgbo@users.noreply.github.com>
2023-12-21 18:12:18 +0800

    up

--- code/now/以二分搜尋法搜尋字串(未完).cpp
+++ code/now/以二分搜尋法搜尋字串(未完).cpp
@@ -2,15 +2,35 @@
 #include<stdlib.h>
 #include<string.h>
 void sort(char zodiac[][40]);
-
+void binary(char zodiac[][40], char key[], int left, int right) {
+	int mid = (right + left) / 2;
+	if (right < left) {
+		printf("沒找到!");
+	}
+	if (strcmp(key,zodiac[mid])==0) {
+		printf("key在:%d", mid);
+	}
+	else if(strlen(key)>strlen(zodiac[mid]))
+	{
+		binary(zodiac, key, mid, right);
+	}
+	else if (strlen(key) < strlen(zodiac[mid]))
+	{
+		binary(zodiac, key, left, mid);
+	}
+	
+}
 int main() {
 	char key[40] = { 0 };
 	char zodiac[12][40] = { "rat", "ox", "tiger", "hare", "dragon", "snake", "horse",
 										"sheep", "monkey", "rooster", "dog", "pig" };
-	scanf("%s", key);
+	sort(zodiac);
+	fgets(key, sizeof(key), stdin);
+	key[strlen(key) - 1] = '\0';
+	binary(zodiac, key, 0, 11);
 }
 
-void sort(char zodiac[][40]) 
+void sort(char zodiac[][40])
 {
 	char temp[40] = { 0 };
 	for (int i = 0; i < 12; i++)
