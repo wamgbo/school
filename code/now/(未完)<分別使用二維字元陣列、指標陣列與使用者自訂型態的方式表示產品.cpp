5bcfda3c4060ff0e6302e0cb4a04dfc25e0d3614
wang_bo <87056038+wamgbo@users.noreply.github.com>
2023-12-31 12:46:32 +0800

    update

---
+++ code/now/(未完)<分別使用二維字元陣列、指標陣列與使用者自訂型態的方式表示產品.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main() {
+	char products[5][30] = { "banana","apple","ketborad","usb","mouse" };
+	int price[5] = { 15,20,2000,300,1499 };
+	const char* ptr[5]= {"banana","apple","ketborad","usb","mouse"};
+
+}
