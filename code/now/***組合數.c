de55ecd8879f0d7fe40d5018d6ba94aafab03a50
wang_bo <87056038+wamgbo@users.noreply.github.com>
2024-01-01 12:15:20 +0000

    update

--- code/now/***組合數.c
+++ code/now/***組合數.c
@@ -17,7 +17,7 @@ int combinatorial_number(int num1,int num2) {
 }
 
 int recursive_combinatorial_number(int num1, int num2) {
-	if (num2 == 0 or num2 == num1) {
+	if (num2 == 0 || num2 == num1) {
 		return 1;
 	}
 	else
