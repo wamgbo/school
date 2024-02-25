78e741ec62d7b1e1f89510c6e192e37ed2942f40
wang_bo <87056038+wamgbo@users.noreply.github.com>
2024-02-25 10:09:50 +0000

    update

---
+++ code/now/second_of_freshman/temp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    string word="香蕉",word1;
+    cin>>word1;
+    if  (strcmp(word,word1)==0)
+    {
+        cout<<"y";
+    }
+    else
+    {
+        cout<<"n";
+    }
+    
+}
