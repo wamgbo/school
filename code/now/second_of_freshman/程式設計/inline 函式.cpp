0097834c5e6f966547f20bb6e311b612b182c2e8
wambo <ccc26732190@gmail.com>
2024-03-04 22:07:48 +0800

    up

---
+++ code/now/second_of_freshman/程式設計/inline 函式.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+inline bool primeQ(int n)//判斷質數
+{
+    int sum = 0;
+    if (n <= 1)
+    {
+        return 0;
+    }
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            sum++;
+        }
+    }
+    if (sum != 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+int main(void)
+{
+    int m, n;
+    while (cin >> m >> n)
+    {
+        int sum = 0;
+        for (int i = m; i <= n; i++)
+        {
+            if (primeQ(i))
+            {
+                sum++;
+            }
+        }
+        cout << sum<<'\n';
+    }
+}
---
+++ code/now/second_of_freshman/程式設計/動態記憶體配置.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+inline void word_to_num(char word[], int size)
+{
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (word[i] >= '0' && word[i] <= '9')
+        {
+            sum += word[i] - '0';
+        }
+    }
+    cout << "字串中之所有阿拉伯數字的和:" << sum;
+}
+int main(void)
+{
+    int num, size;
+    cout << "請輸入字串大小:";
+    cin >> num;
+    getchar();//擷取\n
+    char *p = new char[num];
+    cout << "請輸入字串內容:";
+    cin.getline(p, num);
+    size = strlen(p);//取得動態記憶體輸入內容的字串長度
+    word_to_num(p, size);
+}
