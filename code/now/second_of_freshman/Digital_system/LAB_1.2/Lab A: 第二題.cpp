c6c29397f803e366ab3a078f3ba7e833eb5af417
wambo <ccc26732190@gmail.com>
2024-03-11 17:47:48 +0800

    up

--- code/now/second_of_freshman/Digital_system/LAB_1.2/Lab A: 第二題.cpp
+++ code/now/second_of_freshman/Digital_system/LAB_1.2/Lab A: 第二題.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 using namespace std;
 
-inline void binarytodex(int num, int time, int &dex)
+inline void btod(int num, int time, int &dex)
 {
     int sum = 0;
     for (int i = 0; i < 4; i++)
@@ -12,30 +12,43 @@ inline void binarytodex(int num, int time, int &dex)
     }
     dex = sum;
 }
+void dtob(int num)
+{
+    if (num/2<=0)
+    {
+        cout << num;
+    }
+    else
+    {
+        dtob(num/2);
+        cout << num%2;
+    }
+    
+}
 int main(void)
 {
-    int time, num[4]={0}, num1[4]={0}, dex[4]={0}, dex1[4]={0}, sum1 = 0, sum2 = 0;
-    cout << "位元運算";
-    cin >> time;
+    int time = 4, num[4] = {0}, num1[4] = {0}, dex[4] = {0}, dex1[4] = {0}, sum1 = 0, sum2 = 0;
     cout << "第一位BCD:";
-    for (int i = 0; i < time; i++)
+    for (int i = time - 1; i >= 0; i--)
     {
         cin >> num[i];
     }
     cout << "第二位BCD:";
-    for (int i = 0; i < time; i++)
+    for (int i = time - 1; i >= 0; i--)
     {
         cin >> num1[i];
     }
     for (int i = 0; i < time; i++)
     {
-        binarytodex(num[i], time, dex[i]);
-        binarytodex(num1[i], time, dex1[i]);
+        btod(num[i], time, dex[i]);
+        btod(num1[i], time, dex1[i]);
     }
     for (int i = time; i > 0; i--)
     {
         sum1 += dex[i - 1] * pow(10, i - 1);
         sum2 += dex1[i - 1] * pow(10, i - 1);
     }
-    cout << sum1+sum2 << endl;
+    cout<<"相加結果:";
+    dtob(sum1+sum2);
+    cout << endl;
 }
