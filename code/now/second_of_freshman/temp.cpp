04b4add127442b55c98fab038b0d29ea665ff0d7
wambo <ccc26732190@gmail.com>
2024-03-25 13:33:16 +0800

    up

--- code/now/second_of_freshman/temp.cpp
+++ code/now/second_of_freshman/temp.cpp
@@ -1,17 +1,48 @@
-#include<iostream>
+#include <iostream>
+#include <iomanip>
+#include <string.h>
+#include <cstdlib>
+#include "head.h"
 using namespace std;
 
-int main(void)
+int main()
 {
-    int num;
-    cin>>num;
-    char *ptr=new char[num] {0};
-    ptr[5]='a';
-    for (int i = 0; i < num; i++)
     {
-        cout<<ptr[i];   
+        string a = "John";
+        string b = "William";
+        string c = "Cathy";
+        cout << "\n(1)" << endl;
+        // (1)
+        Account John(a), William(b); // 定義兩個帳戶
+        Account Cathy(c, 12456);
+        // 定義帳戶Cathy
+        Cathy.CheckCount();
+        Cathy.WithDraw(450);
+        William.Deposit(5000);
+        cout << "\n(2)" << endl;
+        cout << "Size of John is:  "
+             << sizeof(John) << endl;
+        John.CheckBalance();
+        Cathy.CheckBalance();
+        William.CheckBalance();
+        John.WithDraw(450);
+        cout << "\n(3)" << endl;
+        {
+            // (2)
+            // (3) 區塊開始
+            // 帳戶 Cathy 提款
+            // 帳戶 Cathy 存款
+            Account Albert("Albert", 1200); // 定義帳戶Albert
+            Albert.CheckCount();
+            Albert.WithDraw(258);
+            // 帳戶Albert 提款
+            Albert.CheckBalance();
+        }
+        Cathy.CheckCount();
+        cout << "\n(4)" << endl;
+        John.CheckRate();
     }
-    
-    
-    delete ptr;
+    getchar()
+    return 0;
+    // (3) 區塊結束
 }
