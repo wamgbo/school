9ff8e15a825bdb39ea0a1dfe7b27e8acb53593e9
wang_bo <87056038+wamgbo@users.noreply.github.com>
2024-04-08 11:43:02 +0800

    Update 圓類別衍生圓柱體類別.cpp

--- code/now/second_of_freshman/Program_design/Job_3/圓類別衍生圓柱體類別.cpp
+++ code/now/second_of_freshman/Program_design/Job_3/圓類別衍生圓柱體類別.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 class Circle
 {
+protected:
     double r;
-    Circle(double input_r)
+public:Circle(double input_r)
     {
         r = input_r;
     }
@@ -12,15 +13,17 @@ class Circle
     {
         return 3.14 * r * r;
     }
-    void printR()
+    double printR()
     {
         return r;
     }
 };
 class Cyn : public Circle
 {
+private:
+    double h;
 public:
-    Cyn(double input_r, double input_h)
+    Cyn(double input_r, double input_h):Circle(input_r)
     {
         r = input_r;
         h = input_h;
@@ -44,7 +47,7 @@ int main()
     cout << "圓面積: " << setprecision(2) << fixed << a_circle.area() << endl; // 輸出  圓面積: 314.00
     cout << "請輸入圓柱體之半徑及高 :";
     cin >> r >> h;                                                               // 設輸入 20 及5
-    Cylinder a_cy(r, h);                                                         // 建立圓柱體物件 a_cylinder, 其半徑為 20 高為 5
+    Cyn a_cy(r, h);                                                         // 建立圓柱體物件 a_cylinder, 其半徑為 20 高為 5
     cout << "圓柱體半徑: " << setprecision(2) << fixed << a_cy.printR() << endl; // 輸出 半徑: 20.00
     cout << "圓柱體面積: " << setprecision(2) << fixed << a_cy.area() << endl;   // 輸出  面積: 3140.00
     cout << "圓柱體體積: " << setprecision(2) << fixed << a_cy.vol() << endl;    // 輸出  面積:6280.00
