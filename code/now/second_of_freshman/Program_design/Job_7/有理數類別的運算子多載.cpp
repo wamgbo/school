bfd8e1c8e8447b3a00203a5d3f2e0d219056c86b
wamgbo <ccc26732190@gmail.com>
2024-06-02 16:18:53 +0800

    up

--- code/now/second_of_freshman/Program_design/Job_7/有理數類別的運算子多載.cpp
+++ code/now/second_of_freshman/Program_design/Job_7/有理數類別的運算子多載.cpp
@@ -1,13 +1,50 @@
+#include <iostream>
+using namespace std;
 class RationalNumber
 {
 public:
-    RationalNumber(int = 0, int = 1); // default
-    constructorRationalNumber operator+(RationalNumber &)
-    additionRationalNumber operator++(int); // 多載後加運算子，設後加會先以目前值運算，再將分子與分母各加一
-    RationalNumber operator++();            // 多載前加運算子，設後加會先將分子與分母各加一，再傳出運算
-    bool operator==(RationalNumber);        // 多載== 運算子
-    bool operator!=(RationalNumber);        // 多載!= 運算子
-    void reduction();                       // 找分子與分母的最大公因數以對分子與分母進行化簡
+    RationalNumber(int numerator = 0, int denominator = 1); // default
+    constructorRationalNumber operator+(RationalNumber &num)
+    {
+        RationalNumber t;
+        t.numerator = this->numerator + num.numerator;
+        t.denominator = this->denominator + num.denominator;
+        return t;
+    }
+    additionRationalNumber operator++(int)
+    {
+        RationalNumber t;
+        t = *this;
+        numerator++;
+        denominator++;
+        return t;
+    } // 多載後加運算子，設後加會先以目前值運算，再將分子與分母各加一
+    RationalNumber operator++()
+    {
+        numerator++;
+        denominator++;
+        return t;
+    } // 多載前加運算子，設後加會先將分子與分母各加一，再傳出運算
+    bool operator==(RationalNumber &num)
+    {
+        return numerator == num.numerator && denominator == num.denominator ? 1 : 0;
+    } // 多載== 運算子
+    bool operator!=(RationalNumber &num)
+    {
+        return numerator == num.numerator && denominator == num.denominator ? 0 : 1;
+    } // 多載!= 運算子
+    void reduction()
+    {
+        int n1 = numerator, n2 = denominator;
+        while (n1 != 0)
+        {
+            int temp = n1;
+            n1 = n2 % n1;
+            n2 = temp;
+        }
+        numerator /= n2;
+        denominator /= n2;
+    } // 找分子與分母的最大公因數以對分子與分母進行化簡
 private:
     int numerator;   // 分子
     int denominator; // 分母
@@ -15,6 +52,11 @@ private:
     friend istream &operator>>(istream &input, RationalNumber &number);
     friend bool operator>(double a, const RationalNumber &number);
 };
+ostream &operator<<(ostream &output, RationalNumber &number)
+{
+    output
+}
+
 
 int main()
 {
